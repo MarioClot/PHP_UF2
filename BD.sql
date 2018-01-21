c8805e99a912b94b69acd6ff3dffddba0abb7e66
MarioClot <mariorrsv@gmail.com>
2018-01-21 12:28:16 +0100

    arreglar .gitignore

--- BD.sql
+++ BD.sql
@@ -11,8 +11,8 @@ CREATE TABLE usuaris
 (codi INT(10) AUTO_INCREMENT,
 nom CHAR(30),
 contrasenya BLOB, /*https://zinoui.com/blog/storing-passwords-securely */ 
-email VARCHAR(20),
-ROL ENUM('admin','professor','alumne') DEFAULT 'alumne',
+email VARCHAR(30),
+ROL ENUM('professor','alumne') DEFAULT 'alumne',
 CONSTRAINT U_usuaris UNIQUE(codi), 
 CONSTRAINT PK_usuaris PRIMARY KEY(codi)
 );
@@ -20,6 +20,9 @@ CONSTRAINT PK_usuaris PRIMARY KEY(codi)
 CREATE TABLE stock
 (codi INT(10) AUTO_INCREMENT,
 nom VARCHAR(30),
+tipus_stock ENUM('fungible','fix') DEFAULT 'fungible',
+ambit VARCHAR(50),
+data_obertura DATE,
 quantitat_inicial FLOAT,
 quantitat_actual FLOAT,
 /* link_compra VARCHAR(255) podriem posar a la row un boto a dins que porti a la pagina on comprar els productes, si dona temps nomes */
@@ -41,4 +44,15 @@ CONSTRAINT U_moviments UNIQUE(codi),
 CONSTRAINT PK_moviments PRIMARY KEY(codi),
 CONSTRAINT FK_moviment_stock FOREIGN KEY (producte) REFERENCES stock(codi),
 CONSTRAINT FK_moviment_usuaris FOREIGN KEY (usuari) REFERENCES usuaris(codi)
+);
+
+CREATE TABLE proveidors
+(codi INT(10),
+nom CHAR(30),
+email VARCHAR(30),
+direccio VARCHAR(100),
+telefon INT(20),
+link VARCHAR(100),
+CONSTRAINT U_proveidors UNIQUE(codi),
+CONSTRAINT PK_proveidors PRIMARY KEY(codi),
 );
