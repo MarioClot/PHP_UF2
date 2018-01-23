fbe5320453857ff87bbf857c98b4ec267c8efde5
MarioClot <mariorrsv@gmail.com>
2018-01-23 22:56:33 +0100

    Prova reconfiguracio git

--- Login/resources/views/welcome.blade.php
+++ Login/resources/views/welcome.blade.php
@@ -89,6 +89,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">PROVA</a>
                 </div>
             </div>
         </div>
